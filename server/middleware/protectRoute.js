8a579dce3dd757a8876cbab76e6a0fb19ad688fa
Riyadh <106389622+Tabrid@users.noreply.github.com>
2024-03-05 03:19:10 +0600

    Add files via upload

--- server/middleware/protectRoute.js
+++ server/middleware/protectRoute.js
@@ -3,7 +3,7 @@ import User from "../models/user.model.js";
 
 const protectRoute = async (req, res, next) => {
 	try {
-		const token = req.cookies.jwt;
+		const token = req?.cookies?.token;
 		console.log(token);
 		if (!token) {
 			return res.status(401).json({ error: "Unauthorized - No Token Provided" });
@@ -14,7 +14,6 @@ const protectRoute = async (req, res, next) => {
 		if (!decoded) {
 			return res.status(401).json({ error: "Unauthorized - Invalid Token" });
 		}
-
 		const user = await User.findById(decoded.userId).select("-password");
 
 		if (!user) {
