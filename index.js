332704dee494e261fa0f1a7a3a2ebf860d30b6d4
Riyadh <106389622+Tabrid@users.noreply.github.com>
2024-03-05 02:46:44 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 
 dotenv.config();
-app.use(cors());
+app.use(cors())
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 app.use("/uploads", express.static("uploads"));
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -35,7 +35,7 @@ export const signup = async (req, res) => {
 
 		if (newUser) {
 			// Generate JWT token here
-			generateTokenAndSetCookie(newUser._id, res);
+			const token = generateTokenAndSetCookie(newUser._id, res);
 			await newUser.save();
 
 			res.status(201).json({
@@ -44,7 +44,8 @@ export const signup = async (req, res) => {
 				username: newUser.username,
 				profilePic: newUser.profilePic,
 				balance: newUser.balance,
-				role: newUser.role
+				role: newUser.role,
+				token: token,
 			});
 		} else {
 			res.status(400).json({ error: "Invalid user data" });
@@ -65,7 +66,7 @@ export const login = async (req, res) => {
 			return res.status(400).json({ error: "Invalid username or password" });
 		}
 
-		generateTokenAndSetCookie(user._id, res);
+		const token = generateTokenAndSetCookie(user._id, res);
 
 		res.status(200).json({
 			_id: user._id,
@@ -73,7 +74,8 @@ export const login = async (req, res) => {
 			username: user.username,
 			profilePic: user.profilePic,
 			balance: user.balance,
-			role: user.role
+			role: user.role,
+			token: token,
 		});
 	} catch (error) {
 		console.log("Error in login controller", error.message);
